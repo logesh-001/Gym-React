9d3b14e0e10ebd5d054e09de0d855a211a32aa35
Senthilnathan <ssenthil1490@outlook.com>
2023-12-29 23:13:55 +0530

    component added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>My React App</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
